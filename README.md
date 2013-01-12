124c59f5ebd4e5ba49a3a433bec95dc670a48621
Tyler Johnston <tylerjohnst@gmail.com>
2013-01-12 17:46:33 -0500

    Changes

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ dotfiles
 ========
 
 Run ./bootstrap.sh to setup.
+
+A bunch of things are sourced from: https://github.com/mathiasbynens/dotfiles
--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ echo 'Setting up dotfiles.'
 ln -f .zshrc ~/.zshrc
 ln -f theme.zsh-theme ~/.oh-my-zsh/themes/custom.zsh-theme
 
-for file in .{gitattributes,gitconfig}; do
+for file in .{gitattributes,gitconfig,gitignore}; do
 	echo "$file"
 	[ -r "$file" ] && ln -f $file ~/$file
 done
