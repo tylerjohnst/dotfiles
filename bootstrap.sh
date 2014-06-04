8d1c535ec2c394dead329cd608a97e6694655002
Tyler Johnston <tylerjohnst@gmail.com>
2014-06-04 10:14:14 -0400

    Simplify vimrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,14 +2,11 @@
 
 echo 'Setting up dotfiles.'
 
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
 for file in .{zshrc,vimrc,gitignore,rspec,gemrc,irbrc,ackrc}; do
 	echo "$file"
 	[ -r "$file" ] && ln -f $file ~/$file
 done
-
-vim -c 'BundleInstall' -c 'q!'
 
 touch ~/.private_exports
 
