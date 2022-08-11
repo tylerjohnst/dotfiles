b53304689af79008b82ec94c4a43cec6fcfc803e
Tyler Johnston <tylerjohnst@gmail.com>
2022-08-11 09:22:14 -0400

    Install extensions

--- install.sh
+++ install.sh
@@ -3,3 +3,9 @@
 export DOTFILES_ROOT=$(pwd -P)
 
 cat zshrc.sh >> ~/.zshrc
+
+# Install VSCode extensions
+cat vscode-extensions.txt | while read extension || [[ -n $extension ]];
+do
+  code --install-extension $extension --force
+done
